7c1e2c5ba1298e9d9e593d4fda4019a1a9abb899
marcossbento <marcossbento14@gmail.com>
2026-01-08 17:36:07 -0300

    Initial Commit

---
+++ src/main/java/com/marcossbento/room_booking_api/RoomBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcossbento.room_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcossbento/room_booking_api/RoomBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcossbento.room_booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
